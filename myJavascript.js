31b50533dd3defc3324503bb8a9af145a67c91d9
Rutjake <77888584+Rutjake@users.noreply.github.com>
2022-02-08 18:05:16 +0200

    Alaotsikko + myJavascript file
    Ominaisuus joka vaihtaa alaotsikon kun valitsee auton tai tyhjän listan

---
+++ myJavascript.js
@@ -0,0 +1,10 @@
+/* Change Model to h2 topic */
+function toyotaInfo(){
+    document.getElementsByTagName('h3')[0].innerHTML = "Toyota Camry 2.2 16v 1996-2001";
+}
+function peugeotInfo(){
+    document.getElementsByTagName('h3')[0].innerHTML = "Peugeot 607 2.2 16v 2005-2011";
+}
+function emptyList(){
+    document.getElementsByTagName('h3')[0].innerHTML = "Choose car or add items in list";
+}
